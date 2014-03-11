495d81afa82c62f7d0ef3dd3a8ca2850e55a3fd7
P. Rego <pvrego@riseup.net>
2014-03-11 00:34:32 -0300

    Adapt USART example to the new adaino memory map architecture.

--- examples/usart_polling_echo/src/main.adb
+++ examples/usart_polling_echo/src/main.adb
@@ -1,26 +1,26 @@
-with AVR.MCU.USART;
+with AVR.USART;
 
 procedure Main is
    Out_Flag_Char : Character;
 
 begin
 
-   AVR.MCU.USART.Initialize
-     (In_Port  => AVR.MCU.USART.USART0,
+   AVR.USART.Initialize
+     (In_Port  => AVR.USART.USART0,
       In_Setup =>
-        (Sync_Mode    => AVR.MCU.USART.ASYNCHRONOUS,
+        (Sync_Mode    => AVR.USART.ASYNCHRONOUS,
          Double_Speed => True,
          Baud_Rate    => 9600,
-         Data_Bits    => AVR.MCU.USART.BITS_8,
-         Parity       => AVR.MCU.USART.NONE,
+         Data_Bits    => AVR.USART.BITS_8,
+         Parity       => AVR.USART.NONE,
          Stop_Bits    => 1,
-         Model        => AVR.MCU.USART.POLLING));
+         Model        => AVR.USART.POLLING));
 
-   AVR.MCU.USART.Put_Line (Data => "#### Initialization ok. ####");
+   AVR.USART.Put_Line (Data => "#### Initialization ok. ####");
 
    loop
-      Out_Flag_Char := AVR.MCU.USART.Get;
-      AVR.MCU.USART.Put (Data => Out_Flag_Char);
+      Out_Flag_Char := AVR.USART.Get;
+      AVR.USART.Put (Data => Out_Flag_Char);
    end loop;
 
 end Main;
