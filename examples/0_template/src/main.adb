f163bbbcf1ce417a04cfa518b66e5e79474f708d
P. Rego <pvrego@riseup.net>
2014-03-15 15:04:12 -0300

    Null project.

--- examples/0_template/src/main.adb
+++ examples/0_template/src/main.adb
@@ -1,26 +1,6 @@
-with AVR.USART;
-
 procedure Main is
-   Out_Flag_Char : Character;
-
 begin
 
-   AVR.USART.Initialize
-     (In_Port  => AVR.USART.USART0,
-      In_Setup =>
-        (Sync_Mode    => AVR.USART.ASYNCHRONOUS,
-         Double_Speed => True,
-         Baud_Rate    => 9600,
-         Data_Bits    => AVR.USART.BITS_8,
-         Parity       => AVR.USART.NONE,
-         Stop_Bits    => 1,
-         Model        => AVR.USART.POLLING));
-
-   AVR.USART.Put_Line (Data => "#### Initialization ok. ####");
-
-   loop
-      Out_Flag_Char := AVR.USART.Get;
-      AVR.USART.Put (Data => Out_Flag_Char);
-   end loop;
+   null;
 
 end Main;
