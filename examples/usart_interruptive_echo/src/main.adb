d0d388f33c63ff50c6e638d47b1ac596de1c00c9
P. Rego <pvrego@riseup.net>
2014-03-15 00:41:46 -0300

    Code for ATmega2560.

--- examples/usart_interruptive_echo/src/main.adb
+++ examples/usart_interruptive_echo/src/main.adb
@@ -1,15 +1,18 @@
+#if MCU="ATMEGA2560" then
 with AVR.USART;
 with AVR.INTERRUPTS;
+#end if;
 
 procedure Main is
---     Out_Flag_Char : Character;
+   --     Out_Flag_Char : Character;
 
 begin
+#if MCU="ATMEGA2560" then
 
    AVR.INTERRUPTS.Disable;
 
    AVR.USART.Initialize
-     (In_Port  => AVR.USART.USART0,
+     (In_Port  => AVR.USART.USART1,
       In_Setup =>
         (Sync_Mode    => AVR.USART.ASYNCHRONOUS,
          Double_Speed => True,
@@ -19,15 +22,21 @@ begin
          Stop_Bits    => 1,
          Model        => AVR.USART.INTERRUPTIVE));
 
-   AVR.USART.Put_Line (Data => "#### Initialization ok. ####");
+   AVR.USART.Put_Line
+     (Port => AVR.USART.USART1,
+      Data => "#### Initialization ok. ####");
 
    AVR.INTERRUPTS.Enable;
 
    loop
-      --Out_Flag_Char := AVR.USART.Get;
-      --AVR.USART.Put (Data => Out_Flag_Char);
+      --        Out_Flag_Char := AVR.USART.Get;
+      --        AVR.USART.Put
+      --          (Port => AVR.USART.USART1,
+      --           Data => Out_Flag_Char);
       null;
-
    end loop;
 
+#else
+   null;
+#end if;
 end Main;
