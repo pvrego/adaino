d29e4d2c2115e0c84237f25110eee66e58094db6
P. Rego <pvrego@riseup.net>
2014-03-16 01:57:55 -0300

    Formatting.

--- examples/usart_interruptive_echo_issue_dbg/src/simple_isr_usrt.c
+++ examples/usart_interruptive_echo_issue_dbg/src/simple_isr_usrt.c
@@ -5,21 +5,16 @@
 #define USART_BAUDRATE 9600
 #define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
 
-int main (void)
-{
+int main (void){
 	UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);
 	UCSR0C = (1 << UCSZ00) | (1 << UCSZ01);
-
 	UBRR0H = (BAUD_PRESCALE >> 8);
 	UBRR0L = BAUD_PRESCALE;
-
 	sei();
-
 	while (1) {}
 }
 
-ISR(USART0_RX_vect)
-{
+ISR(USART0_RX_vect){
 	char ReceivedByte;
 	ReceivedByte = UDR0;
 	UDR0 = ReceivedByte;
